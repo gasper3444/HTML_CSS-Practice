44af000019cac8ff2c41430ed856d19d830e5492
jordanmac <jordanchong@Jordans-MacBook-Pro.local>
2023-07-12 03:16:35 -0700

    template

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import Header from "./Components/Header";
+
+
+function App() {
+  return (
+    <div>
+      Test <i className="fa-solid fa-dragon"></i>
+      <Header/>
+      <section></section>
+      <section></section>
+      <section></section>
+      <section></section>
+      <section></section>
+
+    </div>
+  );
+}
+
+export default App;
